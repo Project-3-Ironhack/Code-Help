f69abdeeac339656acdefdd7aab71249a966982b
Stuart Gardner <stuartogardner@gmail.com>
2017-10-25 19:28:32 +0200

    moving the code editor and creating a Lesson page

---
+++ client/src/router/Lesson.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Let's start your lesson!</h1>
+    <a href="javascript:tagoveApp.max()">Click here to chat with your teacher</a>
+    <!-- <div id="visitor-list"></div> -->
+    <br>
+    <h2>Select a language below to use our code editor</h2>
+    
+    <select v-model="lang">
+      <option disabled value="">Please select one</option>
+      <option>JavaScript</option>
+      <option>HTML</option>
+      <option>CSS</option>
+    </select>
+    <span>Selected: {{ lang }}</span>
+    <br>
+    <br>
+    <AceEditor :lang="lang.toLowerCase()"></AceEditor>
+  </div>
+</template>
+
+<script>
+// window.tagove=window.tagove || [];
+//     tagove.push(function(app){
+//         app.initVisitorNode(document.getElementById('visitor-list'));
+//     });
+import AceEditor from '@/components/Editor'
+
+export default {
+  components: { AceEditor },
+  data () {
+    return {
+      lang: '',
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
