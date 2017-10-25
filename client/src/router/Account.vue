10c1f0d2607df1f463740da0b2e243d6fd1592d1
Stuart Gardner <stuartogardner@gmail.com>
2017-10-25 21:01:04 +0200

    added account holding page

---
+++ client/src/router/Account.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <h2>Welcome to your account</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+}
+</script>
+
+<style>
+
+</style>
