18bf757f4ec0e0a7ff798983294bc59337eca136
Thibaut Davoult <thibaut.davoult@gmail.com>
2017-10-27 14:47:23 +0200

    styling search results and populating skill tags

--- server/bin/seed.js
+++ server/bin/seed.js
@@ -5,39 +5,71 @@ mongoose.connect(process.env.MONGODB_URI,{useMongoClient: true});
 
 const teachersData = [
   {
-    name: "Rob",
+    name: "Yob",
     image: "http://placeholder/url/ph.jpg",
     description: "A nice teacher who only knows JS",
     skills: [
       {
         name: "Javascript",
-        icon: "js-image"
+        icon: "js-image",
+      },
+      {
+        name: "Vue",
+        icon: "vue-image",
+      },
+      {
+        name: "HTML",
+        icon: "html-image",
       }
     ],
     role: "Teacher",
     price: 500
   },
   {
-    name: "Bob",
+    name: "Pob",
     image: "http://placeholder/url/ph2.jpg",
     description: "A good teacher",
     skills: [
       {
         name: "Javascript",
         icon: "js-image"
+      },
+      {
+        name: "Redis",
+        icon: "redis-image",
+      },
+      {
+        name: "Vue",
+        icon: "js-image",
+      },
+      {
+        name: "CSS",
+        icon: "CSS-image",
+      },
+      {
+        name: "HTML",
+        icon: "html-image",
       }
     ],
     role: "Teacher",
     price: 300
   },
   {
-    name: "Gob",
+    name: "Fob",
     image: "http://placeholder/url/ph2.jpg",
     description: "A cool teacher",
     skills: [
       {
         name: "Vue.js",
         icon: "vuejs-image"
+      },
+      {
+        name: "Postman",
+        icon: "postman-image",
+      },
+      {
+        name: "MongoDB",
+        icon: "mongodb-image",
       }
     ],
     role: "Teacher",
