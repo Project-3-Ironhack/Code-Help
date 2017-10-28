dd8f158c9d0d593572e612d5fb4d8388df18e5b7
Stuart Gardner <stuartogardner@gmail.com>
2017-10-29 00:14:30 +0200

    amended footer to include our email addresses

--- client/src/App.vue
+++ client/src/App.vue
@@ -31,8 +31,9 @@
     <div class="container">
       <div class="content has-text-centered">
         <p>
-        <strong>Code Help</strong> by Stuart & Thibaut.
+        <strong>Code Help</strong> by Stuart & Thibaut // Get in touch if you're hiring <icon name="heart"></icon>
         </p>
+        <p><a href="mailto:stuartogardner@gmail.com">stuartogardner@gmail.com</a> // <a href="mailto:thibaut.davoult@gmail.com">thibaut.davoult@gmail.com</a></p>
       </div>
     </div>
   </footer>
@@ -86,4 +87,5 @@ export default {
 .account{
   text-transform: capitalize;
 }
+
 </style>
