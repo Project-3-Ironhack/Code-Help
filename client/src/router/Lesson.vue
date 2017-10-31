72dc69a01034292d9caaf21addb7d2bf04f8be7b
Thibaut Davoult <thibaut.davoult@gmail.com>
2017-10-31 20:55:54 +0100

    fixing merge

--- client/src/router/Lesson.vue
+++ client/src/router/Lesson.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Let's start your lesson!</h1>
+
+    <span @click="endLesson">End lesson</span>
+
     <h3>Select a language below to use our code editor</h3>
 
     <select v-model="lang">
@@ -23,15 +26,29 @@
 <script>
 
 import AceEditor from '@/components/Editor'
+import apiSessions from '@/api/sessions'
 
 export default {
   data () {
     return {
         lang: '',
+        session: '',
     }
   },
   components: {
     AceEditor,
+  },
+  methods: {
+    endLesson() {
+      const id = this.$route.params[0]
+      apiSessions.endSession(id).then(session => {
+        console.log("session from view:", session.data.response)
+        this.session = session.data.response;
+        console.log("this.session:", this.session._id)
+      }).then(() => {
+        this.$router.push('/lesson-summary/' + this.session._id)
+      })
+    }
   },
   created(){
     tagoveApp.max();
